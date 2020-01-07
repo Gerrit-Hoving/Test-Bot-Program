17f663cf515669d1b87a5dff01277f89de1fea5b
Raider Robotics <333woodsia@gmail.com>
2020-01-07 16:23:58 -0500

    Initial commit

---
+++ src/main/java/frc/robot/commands/DriveModeSet.java
@@ -0,0 +1,37 @@
+package frc.robot.commands;/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+import frc.robot.subsystems.Drivetrain;
+
+
+public class DriveModeSet extends Command {
+    public DriveModeSet(int mode) {
+        requires(Robot.drivetrain);
+        if(mode == 0) {
+            Drivetrain.tankDrive();
+        }
+
+        if(mode == 0) {
+            Drivetrain.standardDrive();
+        }
+
+        if(mode == 0) {
+            Drivetrain.curvatureDrive();
+        }
+
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return true;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,43 @@
+package frc.robot.subsystems;/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.OI;
+
+
+public class Drivetrain extends Subsystem {
+    //Sparks at 8 and 9, 6 on talon, 0 and 1 on victor
+
+    private static Spark right = new Spark(8);
+    private static Spark left = new Spark(9);
+
+    /*
+    private static Spark frontRight = new Spark(1);
+    private static Spark rearRight = new Spark(2);
+    private static Spark frontLeft = new Spark(3);
+    private static Spark rearLeft = new Spark(4);
+    //static AnalogGyro m_gyro = new AnalogGyro(1);
+    private static SpeedControllerGroup right = new SpeedControllerGroup(frontRight, rearRight);
+    private static SpeedControllerGroup left = new SpeedControllerGroup(frontLeft, rearLeft);
+    */
+
+    private static DifferentialDrive drive = new DifferentialDrive(right, left);
+
+    public static void tankDrive() { drive.tankDrive(OI.m_stick.getX(), OI.s_stick.getX()); }
+
+    public static void curvatureDrive() { drive.curvatureDrive(OI.m_stick.getX(), OI.m_stick.getZ(), true); }
+
+    public static void standardDrive() { drive.arcadeDrive(OI.m_stick.getX(), OI.m_stick.getY()); }
+
+    public void initDefaultCommand() {
+        //setDefaultCommand(new ExampleCommand());
+    }
+}
