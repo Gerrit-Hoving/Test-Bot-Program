5e333fd38355635b9ac0e8f604e6a5152719fb60
Raider Robotics <333woodsia@gmail.com>
2020-01-09 09:23:02 -0500

    Initial commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,13 +7,12 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.BuiltInAccelerometer;
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -24,105 +23,123 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends TimedRobot
 {
-    static Spark right = new Spark(8);
-    static Spark left = new Spark(9);
+    private Command autonomousCommand;
 
-    static DifferentialDrive drive = new DifferentialDrive(right, left);
+    private RobotContainer robotContainer;
 
-    //public static void tankDrive() { drive.tankDrive(OI.m_stick.getX(), OI.s_stick.getX()); }
-
-    //public static void curvatureDrive() { drive.curvatureDrive(OI.m_stick.getX(), OI.m_stick.getZ(), true); }
-
-    //public static void standardDrive() { drive.arcadeDrive(OI.m_stick.getX(), OI.m_stick.getY()); }
-
-    static OI oi;
+    DifferentialDrive drive;
+    Joystick leftStick;
+    Joystick rightStick;
 
     /**
-     * This function is run when the robot is first started up and is
-     * used for any initialization code.
+     * This method is run when the robot is first started up and should be used for any
+     * initialization code.
      */
     @Override
     public void robotInit()
     {
-        oi = new OI();
+        // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+        // autonomous chooser on the dashboard.
+        robotContainer = new RobotContainer();
 
-        //CameraServer.getInstance().startAutomaticCapture();
-
-        //Pushes data from robot to SmartDashboard
-        SmartDashboard.putData("PDP Info", new PowerDistributionPanel(1));
-        SmartDashboard.putData("Accelerometer", new BuiltInAccelerometer());
+        Spark right = new Spark(8);
+        Spark left = new Spark(9);
+        drive = new DifferentialDrive(right, left);
+        rightStick = new Joystick(0);
+        leftStick = new Joystick(1);
 
     }
 
     /**
-     * This function is called once each time the robot enters Disabled mode
-     * (generally when the game ends) You can use it to reset any subsystem
-     * information you want to clear when the robot is disabled.
+     * This method is called every robot packet, no matter the mode. Use this for items like
+     * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+     *
+     * <p>This runs after the mode specific periodic functions, but before
+     * LiveWindow and SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic()
+    {
+        // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+        // commands, running already-scheduled commands, removing finished or interrupted commands,
+        // and running subsystem periodic() methods.  This must be called from the robot's periodic
+        // block in order for anything in the Command-based framework to work.
+        CommandScheduler.getInstance().run();
+
+
+    }
+
+    /**
+     * This method is called once each time the robot enters Disabled mode.
      */
     @Override
     public void disabledInit()
     {
-
     }
 
     @Override
     public void disabledPeriodic()
     {
-        Scheduler.getInstance().run();
     }
 
     /**
-     * This code helps set up the autonomous by accepting input from the
-     * sendableChooser above.
+     * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
      */
     @Override
     public void autonomousInit()
     {
+        autonomousCommand = robotContainer.getAutonomousCommand();
 
+        // schedule the autonomous command (example)
+        if (autonomousCommand != null)
+        {
+            autonomousCommand.schedule();
+        }
     }
 
     /**
-     * This function is called periodically during autonomous.
+     * This method is called periodically during autonomous.
      */
     @Override
     public void autonomousPeriodic()
     {
-
     }
 
     @Override
     public void teleopInit()
     {
-
+        // This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+        if (autonomousCommand != null)
+        {
+            autonomousCommand.cancel();
+        }
     }
 
     /**
-     * This function is called periodically during operator control.
+     * This method is called periodically during operator control.
      */
     @Override
     public void teleopPeriodic()
     {
-        //Sets the primary driving mode
-        //drive.tankDrive(OI.m_stick.getX(), OI.s_stick.getX());
-        drive.arcadeDrive(OI.m_stick.getX(), OI.m_stick.getY());
-        //tankDrive();
-        //standardDrive();
-        //curvatureDrive();
+        drive.tankDrive(leftStick.getY(), rightStick.getY());
+        //drive.arcadeDrive(leftStick.getY(), leftStick.getX());
+    }
+
+    @Override
+    public void testInit()
+    {
+        // Cancels all running commands at the start of test mode.
+        CommandScheduler.getInstance().cancelAll();
     }
 
     /**
-     * This function sets up testing mode
-     */
-    public void testInit() {
-
-    }
-
-    /**
-     * This function is called periodically during test mode.
+     * This method is called periodically during test mode.
      */
     @Override
     public void testPeriodic()
     {
-        //standardDrive();
     }
 }
